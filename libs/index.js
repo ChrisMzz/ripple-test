ece561373f75bc4f3a4ea6a5d362065aa3ed9907
Christopher MAZZERBO <christopher.mazzerbo@gmail.com>
2022-12-27 17:33:54 +0100

    completely scrapped canvas structure, swapping to svg

--- libs/index.js
+++ libs/index.js
@@ -17,10 +17,10 @@ function mouseCoordinates(event){
     mousePosX = event.clientX;
     mousePosY = event.clientY;
     trigger();
-    console.log("pageX: ", event.pageX,
-    "pageY: ", event.pageY,
-    "clientX: ", mousePosX,
-    "clientY:", mousePosY)
+    // console.log("pageX: ", event.pageX,
+    // "pageY: ", event.pageY,
+    // "clientX: ", mousePosX,
+    // "clientY:", mousePosY)
     }
     
     
@@ -34,32 +34,47 @@ function trigger() {
     var max = custom["chance"];
     var rand = Math.floor(Math.random() * max);
     var limiter = Math.floor(Math.random() * max);
+    console.log(rand, limiter)
     if (rand == limiter) {
       myFunction();
     }
   }
   
 
-function draw(centerX, centerY)
+function draw(centerX, centerY, init_rgb)
   {
-  var canvas = document.getElementById('window');
-  if (canvas.getContext)
-  {
-  var ctx = canvas.getContext('2d'); 
-  var X = centerX;
-  var Y = centerY;
-  var R = 45;
-  ctx.beginPath();
-  ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
-  ctx.lineWidth = 3;
-  ctx.strokeStyle = '#FF0000';
-  ctx.stroke();
+  var temp = 1; 
+  var color = 1;
+  var circle = window.createElement("circle");
+  circle.setAttribute("cx", centerX);
+  circle.setAttribute("cx", centerY);
+  circle.setAttribute("fill", "none");
+  while (color >= 0) {
+    temp += .05;
+    circle.setAttribute("r", 9+temp);
+    var display_color = "rgb(" + init_rgb[0]*color + ", " + init_rgb[1]*color + ", " + init_rgb[2]*color + ")";
+    color -= 0.05;
+    circle.setAttribute("stroke", display_color);
   }
+  delete circle
 }
 
+// using https://www.30secondsofcode.org/js/s/hsl-to-rgb
+// note to self, hue is a value between 0 and 360
+const HSLToRGB = (h, s, l) => {
+  s /= 100; // percentage
+  l /= 100; // percentage
+  const k = n => (n + h / 30) % 12;
+  const a = s * Math.min(l, 1 - l);
+  const f = n =>
+    l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+  return [255 * f(0), 255 * f(8), 255 * f(4)];
+};
 
 function myFunction() {
-  draw(mousePosX, mousePosY)
+  var init_hue = Math.random()*360;
+  var init_rgb = HSLToRGB(init_hue, 100, 69)
+  draw(mousePosX, mousePosY, init_rgb)
   console.log("executed a function")
 }
 
