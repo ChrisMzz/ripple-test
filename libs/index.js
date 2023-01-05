723feb6e72f4d16ae58e5d36c260261f8534ce62
Christopher MAZZERBO <christopher.mazzerbo@gmail.com>
2023-01-05 16:27:16 +0100

    testing out settings input

--- libs/index.js
+++ libs/index.js
@@ -1,4 +1,6 @@
 
+// preparing the page -------------------
+
 var mousePosX;
 var mousePosY;
 
@@ -6,20 +8,22 @@ var clicks = 0;
 
 const customData = await fetch("./libs/custom.json");
 var custom = await customData.json();
-console.log(custom);
+// console.log(custom); // set up the customisable variables
+var max = custom["chance"]
 
 
 const soundsData = await fetch("./libs/soundfiles/sounds.json");
 var sounds = await soundsData.json();
-console.log(sounds);
-console.log(sounds["random_scale_select"]);
+// console.log(sounds);
+console.log(sounds["random_scale_select"]); // display possible scales
 
 var scale = sounds["random_scale_select"][Math.floor(Math.random() * 7)+1];
 var transposeValue = Math.floor(Math.random() * 12);
-console.log(scale);
-console.log(transposeValue);
+console.log(scale); // display current scale
+console.log(transposeValue); // display tranpose value
 
 
+ // functions --------------------------------
 
 function randomNote(scale) {
   var note = (sounds["scales"][scale][Math.floor(Math.random() * 7)] + transposeValue-1) % 12 + 1;
@@ -42,18 +46,7 @@ function mouseCoordinates(event){
     // "clientY:", mousePosY)
     }
     
-    
-
-
-var svg1 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-svg1.setAttribute("height",screen.availHeight);
-svg1.setAttribute("width",screen.availWidth);
-svg1.setAttribute("style","background: rgb(0,0,0)");
-document.body.appendChild(svg1);
-svg1.addEventListener('click', () => {clicks += 1;});
-svg1.addEventListener('mousemove', mouseCoordinates);
-
-function atLeastOneClick() {
+  function atLeastOneClick() {
   return (clicks > 0);
 }
 
@@ -61,15 +54,29 @@ function trigger() {
     if (atLeastOneClick() == false) {
       return
     }
-    var max = custom["chance"];
     var rand = Math.floor(Math.random() * max);
     var limiter = Math.floor(Math.random() * max);
     // console.log(rand, limiter)
     if (rand == limiter) {
-      myFunction();
+      positiveTrigger();
     }
   }
   
+function positiveTrigger() {
+  var init_hue = Math.random()*360;
+  var init_rgb = HSLToRGB(init_hue, 100, 69)
+  var i = Math.floor(Math.random() * 5);
+  if (i==4) {
+    var note = randomNote(scale);
+  } else {
+    var note = randomiNote(scale);
+  }
+  var note_path = 'libs/soundfiles/' + note + '.mp3';
+  var audio = new Audio(note_path);
+  audio.play();
+  draw(mousePosX, mousePosY, init_rgb);
+  console.log("Played a " + note + " with a circle of hue " + init_hue + ".");
+}
 
 function draw(centerX, centerY, init_rgb)
   {
@@ -103,22 +110,34 @@ const HSLToRGB = (h, s, l) => {
     l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
   return [255 * f(0), 255 * f(8), 255 * f(4)];
 };
-
-function myFunction() {
-  var init_hue = Math.random()*360;
-  var init_rgb = HSLToRGB(init_hue, 100, 69)
-  var i = Math.floor(Math.random() * 5);
-  if (i==4) {
-    var note = randomNote(scale);
-  } else {
-    var note = randomiNote(scale);
-  }
-  var note_path = 'libs/soundfiles/' + note + '.mp3';
-  var audio = new Audio(note_path);
-  audio.play();
-  draw(mousePosX, mousePosY, init_rgb);
-  console.log("Played a " + note + " with a circle of hue " + init_hue + ".");
+    
+function updateValues(e) {
+  console.log(e);
+  // max = e.target.value;
+  // scale = e.target.value;
 }
+
+
+// building the page ---------------------------------------------
+
+const settings = document.getElementsByClassName('settings'); // implementing the settings box
+settings.addEventListener('change', updateValues);
+const maxInputBox = document.getElementById('chance');
+const scaleInputBox = document.getElementById('scale');
+
+
+
+var svg1 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+svg1.setAttribute("height",screen.availHeight);
+svg1.setAttribute("width",screen.availWidth);
+svg1.setAttribute("style","background: rgb(0,0,0)");
+document.body.appendChild(svg1);
+svg1.addEventListener('click', () => {clicks += 1;});
+svg1.addEventListener('mousemove', mouseCoordinates);
+
+
+
+
 
 
 
